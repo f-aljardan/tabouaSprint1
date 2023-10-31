e5c74a74c6f168f7981bc3866df737ee2c2a94ec
DanahAl <ddanah.mm@gmail.com>
2023-10-31 21:29:56 +0300

    chnage password add note

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -55,7 +55,7 @@ export default function ProfileMenu({ userData }) {
   const validatePassword = () => {
     if (!passwordValidationRegex.test(newPassword)) {
       setPasswordChangeError(
-        'كلمة المرور يجب أن تحتوي على حرف صغير وحرف كبير وحرف خاص ورقم ويجب أن تكون مكونة من 8 أحرف على الأقل.'
+        'كلمة المرور يجب أن تحتوي على حرف صغير وحرف كبير ورمز ورقم ويجب أن تكون مكونة من 8 أحرف على الأقل.'
       );
       return false;
     } else {
@@ -86,11 +86,27 @@ export default function ProfileMenu({ userData }) {
 
   const passwordStrengthIndicator = (
     <div className="text-xs mb-2">
-      {passwordValidationRegex.test(newPassword) ? (
+      
+      {newPassword !== '' ? (
+    passwordValidationRegex.test(newPassword) ? (
+      <span className="text-green-600">كلمة المرور تلبي المتطلبات</span>
+    ) : (
+      <span className="text-red-600">كلمة المرور لا تلبي المتطلبات</span>
+    )
+  ) : null}
+  {
+    /*
+
+
+
+     {(passwordValidationRegex.test(newPassword) && newPassword !== '' && con) ? (
         <span className="text-green-600">كلمة المرور تلبي المتطلبات</span>
       ) : (
         <span className="text-red-600">كلمة المرور لا تلبي المتطلبات</span>
       )}
+    */
+  }
+     
     </div>
   );
 
@@ -188,7 +204,7 @@ export default function ProfileMenu({ userData }) {
           <DialogBody divider className="font-baloo text-right">
             <div className="text-right">
               <Typography variant='small' className='mb-5'>
-                <span>كلمة المرور يجب أن تكون مكونة من ٨ خانات على الأقل و تشمل حرف كبير ، حرف صغير</span>
+                <span>كلمة المرور يجب أن تكون مكونة من ٨ خانات على الأقل و تشمل حرف كبير ، حرف صغير ، ورمز</span>
               </Typography>
               <Input
                 type="password"
